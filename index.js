237a3b69a100104f90b12756d20ed5e05101ed45
Dominik Kundel <dominik.kundel@gmail.com>
2016-08-10 22:07:39 +0200

    First semi working version

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const mdns = require('mdns');
 const Client = require('castv2-client').Client;
 const DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
 const request = require('request');
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const devices = new Map();
+const playlist = [];
 
 const browser = mdns.createBrowser(mdns.tcp('googlecast'));
 
@@ -24,17 +28,59 @@ function deviceRegister(name, address, port) {
         }
 
         devices.set(id, { name, address, port, client, player });
+        resolve(player);
       });
     });
   });
 }
 
+function createEventListeners(player) {
+  player.on('status', function(status) {
+    console.log('status broadcast playerState=%s', status.playerState);
+    if (status.playerState === 'IDLE') {
+      playlist.splice(0, 1);
+      writePlaylist();
+      if (playlist.length > 0) {
+        player.load(playlist[0], { autoplay: true}, (err, status) => {
+          console.log('Play!');
+        });
+      }
+    }
+  });
+}
+
+function writePlaylist() {
+  let songs = playlist.map(p => p.metadata.title).join('\n');
+  let content = `http://dk.ngrok.io/api
+${songs}`
+  fs.writeFileSync(path.join(os.homedir(), 'playlist.txt'), content, 'utf8');
+}
+
+function getMediaObject(name, artist, songUrl, picture) {
+  return {
+    contentId: songUrl,
+    contentType: 'audio/mpeg3',
+    streamType: 'LIVE', // or LIVE
+
+    // Title and cover displayed while buffering
+    metadata: {
+      type: 0,
+      metadataType: 0,
+      title: `${name} - ${artist}`, 
+      images: [
+        { url: picture }
+      ]
+    }        
+  };
+}
+
 browser.on('serviceUp', (service) => {
   console.log('found device "%s" at %s:%d', service.name, service.addresses[0], service.port);
   // ondeviceup(service.addresses[0]);
-  deviceRegister(service.name, service.addresses[0], service.port).then(() => {
+  deviceRegister(service.name, service.addresses[0], service.port).then(player => {
     console.log('Player registered');
-  }).catch((err) => {
+    return player;
+  }).then(createEventListeners).catch((err) => {
     console.error(err.message);
   });
   browser.stop();
@@ -82,8 +128,38 @@ app.get('/api/devices/:id', (req, res, next) => {
   res.send(result);
 });
 
+app.patch('/api/devices/:id', (req, res, next) => {
+  let state = req.body.state.toLowerCase();
+  let device = devices.get(parseInt(req.params.id, 10));
+
+  if (!device) {
+    res.send(500, 'FAIL');
+    return;
+  }
+
+  if (state === 'pause') {
+    device.player.pause((err, state) => {
+      console.log('Pause');
+    });
+    return res.send('PAUSED');
+  } else if (state === 'play') {
+    device.player.play((err, state) => {
+      console.log('Play');
+    });
+    return res.send('PLAY');
+  } else if (state === 'skip') {
+    playlist.splice(0, 1);
+    if (playlist.length > 0) {
+      device.player.load(playlist[0], { autoplay: true }, (err, state) => {        
+      });
+    }
+    writePlaylist();
+  }
+});
+
 app.post('/api/devices/:id/songs', (req, res, next) => {
   let song = req.body.song;
+  let { player } = devices.get(parseInt(req.params.id, 10));
 
   request.get(`https://api.spotify.com/v1/search?q=${song}&type=track&market=DE`, (err, data, body) => {
     if (err || data.statusCode !== 200) {
@@ -104,13 +180,23 @@ app.post('/api/devices/:id/songs', (req, res, next) => {
           return;
         }
         body = JSON.parse(body);
-        console.dir(body, { depth: 1});
 
         if (body) {
           let albumImage = body.album.images[0].url;
           let title = body.name;
           let artist = body.artists[0].name;
           let url = body.preview_url;
+
+          let media = getMediaObject(title, artist, url, albumImage);
+
+          playlist.push(media);
+          writePlaylist();
+
+          if (playlist.length === 1) {
+            player.load(media, { autoplay: true }, (err, status) => {
+              console.log('PLAY');
+            });
+          }
 
           res.send({albumImage, artist, title, url});
         }
